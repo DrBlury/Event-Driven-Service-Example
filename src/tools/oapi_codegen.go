77371eb2031fc714fd1350876ab60c7880afa506
Julian Bensch <bensch.julian@gmail.com>
2025-11-14 01:36:39 +0100

    removed old tools

--- src/tools/oapi_codegen.go
+++
@@ -1,3 +0,0 @@
-//go:generate sh -c "cd .. && docker run --rm -v \"$PWD:/workspace\" -w /workspace drblury/oapi-codegen -config ./pkg/api/server-std.cfg.yml ./internal/server/_gen/openapi.json"
-
-package tools
--- src/tools/tools.go
+++
@@ -1,3 +0,0 @@
-package tools
-
-// This package intentionally left without dependencies; see oapi_codegen.go for go:generate directives.
